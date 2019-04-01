1aa7c4deadc2045f4ce70cd543dbc763d2fafeae
logizta <48102165+logizta@users.noreply.github.com>
2019-04-01 14:52:22 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+var config = {};
+
+config.twitter = {
+  consumer_key: 'ZXkCPjvWHYUvcsHQ9K2N1M6qn',
+  consumer_secret: 'YQW1vNkh5MQHFPE0aTOiTbOBykA93djn4Pc1s5FysRsiQDjmNH',
+  access_token: '1529294611-9y2G7K2NQg7x3XIyMP7nZMZ0RFwMpAEFzNFmb3U',
+  access_token_secret: 'cFGG7T47kKAoXgh1tBj0hXc4DaGkBTfQCmqdDfM6Iru75'
+}
+
+module.exports = config;
